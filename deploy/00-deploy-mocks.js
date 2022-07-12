2990e622ea65aab008d3110118d54b8a76a0ae90
ahad rizwan <ahadrizwan007@gmail.com>
2022-07-12 10:03:38 +0530

    initial commit

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,24 @@
+const { network } = require("hardhat")
+const {
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("MockV3Aggregator", {
+            contract: "MockV3Aggregator",
+            from: deployer,
+            args: [DECIMALS, INITIAL_ANSWER],
+            log: true,
+        })
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,42 @@
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // if chainId is x user address Y
+    // if chainId is z use address A
+
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    // if the contract doesn't exist, we deploy a minimal version of it for our local testing
+
+    // well what happens when we want to change chanis?
+    // when going for loclahost or hardhat network we want to use mock
+    const args = [ethUsdPriceFeedAddress]
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("-------------------------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
